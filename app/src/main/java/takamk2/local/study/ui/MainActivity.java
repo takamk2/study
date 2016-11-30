4dddd45425ba7ab27907a512e3cf2c55169d50a6
takamk2 <y.m.t.m.s.a.s@gmail.com>
2016-11-30 23:13:27 +0900

    Import necessary libraries

--- app/src/main/java/takamk2/local/study/ui/MainActivity.java
+++ app/src/main/java/takamk2/local/study/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package takamk2.local.study;
+package takamk2.local.study.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import takamk2.local.study.R;
 
 public class MainActivity extends AppCompatActivity {
 
