e1de7ac24559b4e3af46eab853a8272cb526e95e
takamk2 <y.m.t.m.s.a.s@gmail.com>
2016-11-30 22:14:42 +0900

    first commit

---
+++ app/src/androidTest/java/takamk2/local/study/ApplicationTest.java
@@ -0,0 +1,13 @@
+package takamk2.local.study;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
